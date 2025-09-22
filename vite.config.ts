c7ce3a1d79ef8fa14db6a92aeba67dba1308580e
Ieva Vyliaudaite <ievavyliaudaite@Ieva-MacBook-Pro.local>
2025-09-22 13:52:31 +0300

    change define in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
     define: {
-      'process.env': env
+      'process.env.production': env
     },
     server: {
       port: 3000,
