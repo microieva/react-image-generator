8de1b47c0a9e9a0e0c9a184312bfaa3dd75db403
Ieva Vyliaudaite <ievavyliaudaite@Ieva-MacBook-Pro.local>
2025-09-23 15:23:22 +0300

    fix timezone in tasks

--- src/hooks/useDateFormatting.ts
+++ src/hooks/useDateFormatting.ts
@@ -1,5 +1,5 @@
 import {DateTime} from 'luxon';
 
 export const useDateFormatting = (date:string) => {
-  return DateTime.fromISO(date).toFormat('HH:mm:ss, dd MMM')
+  return DateTime.fromISO(date).setZone('Europe/Helsinki').toFormat('HH:mm:ss, dd MMM')
 }
