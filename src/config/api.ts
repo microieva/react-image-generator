2668d0c8ae972683d74a8da3138e369e92da54da
Ieva Vyliaudaite <ievavyliaudaite@Ieva-MacBook-Pro.local>
2025-10-01 14:50:44 +0300

    use env.apiBaseUrl

--- src/config/api.ts
+++ src/config/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import env from "../utils/env";
 
 const API_BASE_URL = env.isProduction 
-  ? ''  
+  ? env.apiBaseUrl  
   : 'http://127.0.0.1:8000';  
 
 export const apiClient = axios.create({
--- src/config/ess.ts
+++ src/config/ess.ts
@@ -2,7 +2,7 @@ import env from "../utils/env";
 
 
 const API_BASE_URL = env.isProduction 
-  ? ''  
+  ? env.apiBaseUrl  
   : 'http://127.0.0.1:8000';  
 
 export const createEventSource = (url:string, options = {}) => {
